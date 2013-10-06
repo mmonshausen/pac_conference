beb15ac67e8c7c235a560f8c2d48c2c0b475ed57
mmonshausen <mmonshausen@prodyna.com>
2013-10-06 18:00:48 +0200

    - added additional service method

--- conference/conference-ejb/src/main/java/com/prodyna/pac/mmonshausen/conference/service/SpeakerService.java
+++ conference/conference-ejb/src/main/java/com/prodyna/pac/mmonshausen/conference/service/SpeakerService.java
@@ -1,6 +1,5 @@
 package com.prodyna.pac.mmonshausen.conference.service;
 
-import java.util.Date;
 import java.util.List;
 
 import com.prodyna.pac.mmonshausen.conference.model.Speaker;
@@ -13,7 +12,6 @@ public interface SpeakerService {
 	public Speaker saveSpeaker(Speaker speaker);
 	
 	public Speaker getSpeakerById(long id);
-	public List<Speaker> getSpeakerByDate(Date date);
 	public List<Speaker> listAllSpeakers();
 	
 	public Speaker updateSpeaker(Speaker speaker);
