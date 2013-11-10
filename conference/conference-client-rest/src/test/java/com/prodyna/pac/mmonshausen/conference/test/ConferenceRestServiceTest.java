e8e408718e4049f438abf8934bb4b49032165f66
mmonshausen <mmonshausen@prodyna.com>
2013-11-10 23:47:40 +0100

    -fixed dependencies for test

--- conference/conference-client-rest/src/test/java/com/prodyna/pac/mmonshausen/conference/test/ConferenceRestServiceTest.java
+++ conference/conference-client-rest/src/test/java/com/prodyna/pac/mmonshausen/conference/test/ConferenceRestServiceTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.apache.http.HttpStatus;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -19,7 +18,6 @@ import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -47,13 +45,13 @@ public class ConferenceRestServiceTest {
 		final WebArchive archive = ShrinkWrap
                 .create(WebArchive.class, "test.war")
                 .addPackages(true, "com/prodyna/pac/conference")
-                .deletePackages(true, "com/prodyna/pac/conference/rest/test")
                 .addAsResource("META-INF/test-persistence.xml",
                                 "META-INF/persistence.xml")
                 .addAsResource("META-INF/test-beans.xml", "META-INF/beans.xml")
                 // Deploy our test datasource
                 .addAsWebInfResource("test-ds.xml", "test-ds.xml")
                 .addAsLibraries(libs);
+		return archive;
 	}
 	
 	@ArquillianResource
